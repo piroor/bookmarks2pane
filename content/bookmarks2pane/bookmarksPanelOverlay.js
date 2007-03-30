aaf098335a179067285ba50bf04a175206b4684b
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-03-30 03:12:56 +0000

    ・Boox向けのハックを追加 ・XBLを使用しない実装にした
    git-svn-id: https://www.cozmixng.org/repos/piro/bookmarks2pane/trunk@292 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- content/bookmarks2pane/bookmarksPanelOverlay.js
+++ content/bookmarks2pane/bookmarksPanelOverlay.js
@@ -30,50 +30,19 @@ var Bookmarks2PaneService = {
 		if (this.initialized) return;
 		this.initialized = true;
 
-
 		this.mainTree        = document.getElementById('bookmarks-view');
 		this.contentTree     = document.getElementById('bookmarks-content-view');
 		this.contentLabel    = document.getElementById('bookmarks-content-label');
 		this.contentLabelBox = document.getElementById('bookmarks-content-label-box');
 		this.dustbox         = document.getElementById('bookmarks-dustbox');
 		this.splitter        = document.getElementById('bookmarks-panes-splitter');
+
+		this.overrideTree(this.mainTree, 'main');
+		this.overrideTree(this.contentTree, 'content');
 		this.currentTree     = this.mainTree;
 
-		this.mainTree.addEventListener('click', this.onToggleOpenState, false);
-		this.mainTree.addEventListener('keypress', this.onToggleOpenStateKey, false);
-		this.mainTree.addEventListener('Bookmarks2PaneOnFolderSelect', this.onTargetChange, false);
+		this.hackForOtherExtensions();
 
-
-		// hack for Bookmarks Duplicate Detector
-		if ('BddsearchBookmarks' in window)
-			eval(
-				'window.BddsearchBookmarks = '+
-				window.BddsearchBookmarks.toSource().replace(
-					/(if\s*\(!aInput\))/,
-					'var event = document.createEvent("Events"); event.initEvent("Bookmarks2PaneOnFolderSelect", false, true); $1'
-				).replace(
-					/bookmarkView\.tree\.setAttribute\(\s*['"]ref['"],\s*bookmarkView\.originalRef\s*\)/,
-					'event.targetRef = null'
-				).replace(
-					/bookmarkView\.tree\.setAttribute\(\s*['"]ref['"],/g,
-					'event.targetRef = ('
-				).replace(
-					/\}(\)?)$/,
-					'; Bookmarks2PaneService.mainTree.dispatchEvent(event);}$1'
-				)
-			);
-
-/*
-		// hack for Boox
-		if ('booxBPTooltip' in window)
-			eval(
-				'window.booxBPTooltip.fillInTooltip = '+
-				window.booxBPTooltip.fillInTooltip.toSource().replace(
-					/var tree = document.getElementById("bookmarks-view").tree;/,
-					''
-				)
-			);
-*/
 		window.setTimeout('Bookmarks2PaneService.delayedInit()', 0);
 	},
 	delayedInit : function()
@@ -81,6 +50,7 @@ var Bookmarks2PaneService = {
 		var lastRef = nsPreferences.copyUnicharPref('bookmarks2pane.last_selected_folder') || 'rdf:null';
 		if (lastRef != 'rdf:null') {
 			this.contentTree.setAttribute('ref', lastRef);
+			this.contentTree.tree.setAttribute('ref', lastRef);
 			this.contentTree.treeBuilder.rebuild();
 			this.contentLabel.value = BookmarksUtils.getProperty(RDF.GetResource(lastRef), 'http://home.netscape.com/NC-rdf#Name');
 		}
@@ -115,6 +85,198 @@ var Bookmarks2PaneService = {
 			)
 		);
 	},
+
+
+	overrideTree : function(aTree, aType)
+	{
+		aTree.getRootResource = this.treeImplementations.getRootResource;
+		aTree.openFolderContent = this.treeImplementations.openFolderContent;
+
+		aTree.tree.setAttribute('onclick', 'this.parentNode.openItemClick(event, 1); this.parentNode.openFolderContent(event, 1);');
+		aTree.tree.setAttribute('ondblclick', 'this.parentNode.openItemClick(event, 2); this.parentNode.openFolderContent(event, 2);');
+		aTree.tree.setAttribute('onkeypress', 'if (event.keyCode == 13) { this.parentNode.openItemKey(); this.parentNode.openFolderContent(event); }');
+
+		aTree.tree.setAttribute('onselect',
+			'Bookmarks2PaneService.currentTree = this.parentNode; '+
+			aTree.tree.getAttribute('onselect')
+		);
+		aTree.tree.setAttribute('onfocus', 'Bookmarks2PaneService.currentTree = this.parentNode;');
+		aTree.tree.setAttribute('onmousedown', 'Bookmarks2PaneService.currentTree = this.parentNode;');
+		aTree.tree.setAttribute('onkeydown', 'Bookmarks2PaneService.currentTree = this.parentNode;');
+
+		if (aType == 'content') {
+			aTree.setAttribute('rdf:null');
+			aTree.tree.setAttribute('rdf:null');
+
+			aTree.onFolderContentOpen = this.treeImplementations.onContentTreeFolderContentOpen;
+			aTree.__defineGetter__('label', this.treeImplementations.contentTreeLabelGetter);
+		}
+		else {
+			aTree.tree.setAttribute('ref', 'NC:BookmarksTopRoot');
+			var template = aTree.tree.getElementsByTagName('template')[0];
+			template.lastChild.setAttribute('iscontainer', 'true')
+			while (template.lastChild == template.firstChild)
+				template.removeChild(template.firstChild);
+
+			aTree.searchBookmarks = this.treeImplementations.searchBookmarks;
+			aTree.onFolderContentOpen = this.treeImplementations.onFolderTreeFolderContentOpen;
+
+			aTree.addEventListener('click', this.onToggleOpenState, false);
+			aTree.addEventListener('keypress', this.onToggleOpenStateKey, false);
+			aTree.addEventListener('Bookmarks2PaneOnFolderSelect', this.onTargetChange, false);
+		}
+
+		aTree.treeBuilder.rebuild();
+	},
+
+	treeImplementations : {
+		getRootResource : function()
+		{
+			return RDF.GetResource(this.tree.ref);
+		},
+		openFolderContent : function(aEvent, aClickCount)
+		{
+			if (aClickCount !== void(0) &&
+				aEvent.type != 'keypress') {
+				if (aEvent.button == 2 ||
+					aEvent.originalTarget.localName != 'treechildren')
+					return;
+			}
+
+			var row = {};
+			var col = {};
+			var obj = {};
+			this.treeBoxObject.getCellAt(aEvent.clientX, aEvent.clientY, row, col, obj);
+			row = row.value;
+
+			if (/*row == -1 ||*/ aEvent.type != 'keypress' && obj.value == 'twisty') return;
+
+			if (aEvent.type != 'keypress') {
+				var modifKey = aEvent.shiftKey ||
+								aEvent.ctrlKey ||
+								aEvent.altKey ||
+								aEvent.metaKey  ||
+								aEvent.button == 1;
+				if (this.clickCount == 2 && modifKey) {
+					/*
+						treeBoxObject.view.selection : 2.0-
+						view.selection : 1.8a-1.9
+						treeBoxObject.selection : -1.7.x
+					*/
+					(
+						'view' in this.treeBoxObject ?
+							this.treeBoxObject.view.selection :
+						'selection' in this.treeBoxObject ?
+							this.treeBoxObject.selection :
+							this.view.selection
+						).select(row);
+					this._selection = this.getTreeSelection();
+				}
+			}
+
+			var selection = this._selection;
+			if (!selection || !selection.isContainer[0]) return;
+
+			if (
+				'onFolderContentOpen' in this &&
+				!this.onFolderContentOpen(aEvent, aClickCount, selection)
+				)
+				return;
+		},
+
+
+
+		/* for Folder Tree */
+
+		searchBookmarks : function(aInput)
+		{
+			/*
+				treeBoxObject.view.selection : 2.0-
+				view.selection : 1.8a-1.9
+				treeBoxObject.selection : -1.7.x
+			*/
+			(
+				'view' in this.treeBoxObject ?
+					this.treeBoxObject.view.selection :
+				'selection' in this.treeBoxObject ?
+					this.treeBoxObject.selection :
+					this.tree.view.selection
+				).currentIndex = -1;
+
+			var event = document.createEvent('Events');
+			event.initEvent('Bookmarks2PaneOnFolderSelect', false, true);
+
+			if (!aInput) {
+				event.targetRef = null;
+			}
+			else {
+				event.targetRef = 'find:datasource=rdf:bookmarks&match=http://home.netscape.com/NC-rdf#Name&method=contains&text=' + escape(aInput);
+			}
+
+			this.dispatchEvent(event);
+		},
+
+		onFolderTreeFolderContentOpen : function(aEvent, aClickCount, aSelection)
+		{
+			var modifKey = aEvent.shiftKey ||
+							aEvent.ctrlKey ||
+							aEvent.altKey ||
+							aEvent.metaKey  ||
+							aEvent.button == 1;
+
+			if (
+				aEvent.type == 'keypress' ||
+				(
+				aClickCount == 1 &&
+				!modifKey
+				)
+				) {
+				if (
+					aSelection.length != 1 ||
+					('protocol' in aSelection && aSelection.protocol[0] != 'file')
+					)
+					return false;
+			}
+
+			var event = document.createEvent('Events');
+			event.initEvent('Bookmarks2PaneOnFolderSelect', false, true);
+			event.targetRef  = 'selection';
+			this.dispatchEvent(event);
+
+			return true;
+		},
+
+
+		/* Content Tree */
+
+		onContentTreeFolderContentOpen : function(aEvent, aClickCount, aSelection)
+		{
+				if (
+					(
+					aSelection.type[0] != 'Folder' &&
+					aSelection.type[0] != 'PersonalToolbarFolder'
+					) ||
+					(
+					aEvent.type != 'keypress' &&
+					aClickCount == 1
+					)
+					) {
+					aEvent.stopPropagation();
+					return false;
+				}
+				return true;
+		},
+
+		contentTreeLabelGetter : function()
+		{
+			return document.getAnonymousElementByAttribute(this, 'anonid', 'bookmarks-tree-content-label');
+		}
+
+	},
+
+
+
+
 
 	onToggleOpenStateKey : function(aEvent)
 	{
@@ -194,6 +356,7 @@ var Bookmarks2PaneService = {
 		if (aEvent.targetRef == 'selection') {
 			var selection = Bookmarks2PaneService.mainTree._selection;
 			tree.setAttribute('ref', selection.item[0].Value);
+			tree.tree.setAttribute('ref', selection.item[0].Value);
 			tree.treeBuilder.rebuild();
 
 			Bookmarks2PaneService.contentLabel.value = BookmarksUtils.getProperty(selection.item[0], 'http://home.netscape.com/NC-rdf#Name');
@@ -254,6 +417,46 @@ var Bookmarks2PaneService = {
 			flavours.appendFlavour('moz/rdfitem');
 			return flavours;
 		}
+	},
+
+
+
+	hackForOtherExtensions : function()
+	{
+		// hack for Bookmarks Duplicate Detector
+		if ('BddsearchBookmarks' in window)
+			eval(
+				'window.BddsearchBookmarks = '+
+				window.BddsearchBookmarks.toSource().replace(
+					/(if\s*\(!aInput\))/,
+					'var event = document.createEvent("Events"); event.initEvent("Bookmarks2PaneOnFolderSelect", false, true); $1'
+				).replace(
+					/bookmarkView\.tree\.setAttribute\(\s*['"]ref['"],\s*bookmarkView\.originalRef\s*\)/,
+					'event.targetRef = null'
+				).replace(
+					/bookmarkView\.tree\.setAttribute\(\s*['"]ref['"],/g,
+					'event.targetRef = ('
+				).replace(
+					/\}(\)?)$/,
+					'; Bookmarks2PaneService.mainTree.dispatchEvent(event);}$1'
+				)
+			);
+
+
+		// hack for Boox
+		if ('booxBPTooltip' in window)
+			eval(
+				'window.booxBPTooltip.fillInTooltip = '+
+				window.booxBPTooltip.fillInTooltip.toSource().replace(
+					'var tree = document.getElementById("bookmarks-view").tree;',
+					'var tree = document.tooltipNode || document.popupNode;'+
+					'while (tree.parentNode && tree.localName != "bookmarks-tree") {'+
+						'tree = tree.parentNode;'+
+					'};'+
+					'if (!tree) { return false; } else { tree = tree.tree; };'
+				)
+			);
+
 	}
 
 };
